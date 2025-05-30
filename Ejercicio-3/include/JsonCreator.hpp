92a5f2e7fc0120c258ce3d2b1ea59b663fc16e72
Santino Cardelli <sancardelli@gmail.com>
2025-05-30 18:03:19 -0300

    Cambios finales en Ejercicio 3

--- Ejercicio-3/include/JsonCreator.hpp
+++ Ejercicio-3/include/JsonCreator.hpp
@@ -4,14 +4,10 @@
 
 /**
  * Clase encargada de construir el JSON final.
- * Almacena internamente las cadenas formateadas para cada sección del JSON,
- * y provee métodos para procesar vectores/matrices de distintos tipos.
+ * Almacena internamente las cadenas formateadas para cada sección del JSON
 */
 class JsonCreator{
     private:
-        std::string doubleString; // String con el contenido JSON para "vec_doubles"
-        std::string palabras; // String con el contenido JSON para "palabras"
-        std::string listas; // String con el contenido JSON para "listas"
         std::string jsonStr;
 
         template <typename T>
@@ -30,37 +26,9 @@ class JsonCreator{
             }
         }
 
-        void printDoubles() const{
-            std::cout<<"\"vec_doubles\" : [";
-            if(!doubleString.empty()){
-                std::cout<<doubleString;
-            }else{
-                std::cout<<"],\n";
-            }
-        }
-        // Métodos privados para imprimir cada sección con las etiquetas JSON
-
-        void printPalabras() const{
-            std::cout<<"  \"palabras\" : [";
-            if(!palabras.empty()){
-                std::cout<<palabras;
-            }else{
-                std::cout<<"],\n";
-            }
-        }
-
-        void printListas() const{
-            std::cout<<"  \"listas\" : [\n";
-            if(!listas.empty()){
-                std::cout<<listas;
-            }else{
-                std::cout<<"          ]\n";
-            }
-        }
-
     public:
         /**
-         * Constructor variádico que recibe múltiples Data<T> y los procesa.
+         * Constructor que recibe multiples Data<T> y los procesa.
          * Permite pasar en cualquier orden los distintos tipos de datos.
         */
         template <typename... Args>
